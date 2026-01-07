bf9486bb9891e0b74d47958bf910c735f99cfb80
Mario <marinaldo12@hotmail.com>
2026-01-07 16:04:36 -0300

    adição de url do due

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView,SpectacularRedocView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('auth.urls')),
     path('api/v1/oficio/', include('oficio.urls')),
+    path('api/v1/due/', include('due.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
