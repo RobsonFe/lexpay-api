7892f2538f434ea05060de54dbe619c3e6903815
Mario <marinaldo12@hotmail.com>
2026-01-08 14:22:43 -0300

    view com nova nomenclatura de permissao

--- due/views.py
+++ due/views.py
@@ -1,15 +1,15 @@
 from rest_framework import generics
-from due.permissions import IsAnalistOrAdmin 
-from due.models import DueDiligence
+from due.permissions import IsBrokerOrAdmin 
+from due.models import DueDiligence, TypeUserChoices
 from due.serializer import DueDiligenceSerializer
 
 class DueDiligenceListCreateView(generics.ListCreateAPIView):
     serializer_class = DueDiligenceSerializer
-    permission_classes = [IsAnalistOrAdmin]
+    permission_classes = [IsBrokerOrAdmin]
 
     def get_queryset(self):
         user = self.request.user
-        if user.type_user == 'Administrador':
+        if user.type_user == TypeUserChoices.ADMINISTRADOR:
             return DueDiligence.objects.all()
         return DueDiligence.objects.filter(analista=user)
     
@@ -19,7 +19,7 @@ class DueDiligenceListCreateView(generics.ListCreateAPIView):
 
 class DueDiligenceRetrieveUpdateView(generics.RetrieveUpdateAPIView):
     serializer_class = DueDiligenceSerializer
-    permission_classes = [IsAnalistOrAdmin]
+    permission_classes = [IsBrokerOrAdmin]
     lookup_field = 'pk'
 
     def get_queryset(self):
