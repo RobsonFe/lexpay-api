dae0774710c813a22057059547dca0786f7be643
spatialcaver <evertongabrielljs@gmail.com>
2026-01-08 14:01:31 -0300

    subindo novas correções

--- proposal/serializer.py
+++ proposal/serializer.py
@@ -7,7 +7,7 @@ class ProposalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Proposal
         fields = 'id, status, valor_proposto, taxa_desconto, taxa_juros_anual, prazo_pagamento_meses, valor_liquido_cedente, valor_liquido_proponente, data_vencimento', 'margem_lucro_percentual'
-        read_only_fields = ('id', 'created_at', 'update_at')
+        read_only_fields = ('id', 'created_at', 'updated_at')
 
 class ProposalHistorySerializer(serializers.ModelSerializer):
     class Meta:
