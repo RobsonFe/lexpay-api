85963f26ab6c66909a725c3560c2c4b44f83b7f0
spatialcaver <evertongabrielljs@gmail.com>
2026-01-08 13:44:33 -0300

    subindo novas correções

--- proposal/models.py
+++ proposal/models.py
@@ -7,13 +7,21 @@ from due.models import DueDiligence
 # Create your models here.
 
 
-STATUS = [
-    ('RASCUNHO', 'rascunho'),
-    ('ENVIADA', 'enviada'),
-    ('ACEITA', 'aceita'),
-    ('REJEITADA', 'rejeitada'), 
-    ('EXPIRADA', 'expirada')
-]
+class StausChoices:
+    
+    RASCUNHO = 'Rascunho'
+    ENVIADA = 'Enviada'
+    ACEITA = 'Aceita'
+    REJEITADA = 'Rejeitada'
+    EXPIRADA = 'Expirada'
+
+    STATUS = [
+        ('RASCUNHO', 'rascunho'),
+        ('ENVIADA', 'enviada'),
+        ('ACEITA', 'aceita'),
+        ('REJEITADA', 'rejeitada'), 
+        ('EXPIRADA', 'expirada')
+    ]
 
 
 
@@ -23,15 +31,14 @@ STATUS = [
 class Proposal(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     precatorio = models.ForeignKey(Precatorio, on_delete=models.PROTECT)
-    proponente = models.ForeignKey(User, on_delete=models.PROTECT)
+    proponente = models.ForeignKey(User, on_delete=models.PROTECT, limit_choices_to={'type_user': 'Broker'})
     valor_proposto = models.DecimalField(max_digits=18, decimal_places=2)
     taxa_desconto = models.DecimalField(max_digits=5, decimal_places=2)
     taxa_juros_anual = models.DecimalField(max_digits=5, decimal_places=2)
     prazo_pagamento_meses = models.IntegerField()
     valor_liquido_cedente = models.DecimalField(max_digits=18, decimal_places=2)
     valor_liquido_proponente = models.DecimalField(max_digits=18, decimal_places=2)
-    margem_lucro_percentual = models.DecimalField(max_digits=5, decimal_places=2)
-    status = models.CharField(max_length=20, choices=STATUS, default='RASCUNHO')
+    status = models.CharField(max_length=20, choices=StausChoices.STATUS, default='RASCUNHO')
     data_vencimento = models.DateField()
     observacoes = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -39,6 +46,15 @@ class Proposal(models.Model):
     
     _current_user = None
     
+    
+    @property
+    def margem_lucro_percentual(self):
+        if self.valor_proposto > 0:
+            valor_face = self.precatorio.valor_principal
+            
+            return ((valor_face - self.valor_proposto) / self.valor_proposto) * Decimal('100')
+        
+        return 0
     
     def __str__(self):
         return f"Proposal: {self.id} - {self.status}, Proponente: {self.proponente} Valor: {self.valor_proposto} Vencimento: {self.data_vencimento}"
@@ -109,8 +125,8 @@ class Proposal(models.Model):
 class ProposalHistory(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     proposal = models.ForeignKey(Proposal, on_delete=models.CASCADE)
-    status_anterior = models.CharField(max_length=20, choices=STATUS, default='RASCUNHO')
-    status_novo = models.CharField(max_length=20, choices=STATUS, default='ENVIADA')
+    status_anterior = models.CharField(max_length=20, choices=StausChoices.STATUS, default='RASCUNHO')
+    status_novo = models.CharField(max_length=20, choices=StausChoices.STATUS, default='ENVIADA')
     valor_anterior = models.DecimalField(max_digits=18, decimal_places=2, null=False, blank=False)
     valor_novo = models.DecimalField(max_digits=18, decimal_places=2, null=False, blank=False)
     alterado_por = models.ForeignKey(User, on_delete=models.PROTECT)
